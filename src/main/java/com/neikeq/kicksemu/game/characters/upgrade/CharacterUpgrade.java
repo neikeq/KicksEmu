e858e7598687c65cbac0c4c6a446390a895e08b3
neikeq <ignalfonsore@gmail.com>
2015-01-01 17:09:42 +0100

    Fix wrong new stats when upgrading character.

--- src/main/java/com/neikeq/kicksemu/game/characters/upgrade/CharacterUpgrade.java
+++ src/main/java/com/neikeq/kicksemu/game/characters/upgrade/CharacterUpgrade.java
@@ -17,15 +17,15 @@ public class CharacterUpgrade {
         getStats().put(PositionCodes.ST,
                 new PlayerStats(0, 0, 0, 7, 7, 0, -10, 7, 7, -10, 0, 0, 0, 0, 0, 0, 0));
         getStats().put(PositionCodes.CF,
-                new PlayerStats(0, 0, 0, 0, 7, 0, -10, 0, 7, -10, 0, 7, 0, 0, 0, 0, 0));
+                new PlayerStats(0, 0, 7, 0, 7, 0, -10, 0, 7, -10, 0, 7, 0, 0, 0, 0, 0));
         getStats().put(PositionCodes.WF,
-                new PlayerStats(0, 7, 7, 0, 7, 0, -10, 0, 7, -10, 7, 0, 0, 0, 0, 0, 0));
+                new PlayerStats(0, 7, 0, 0, 7, 0, -10, 0, 7, -10, 7, 0, 0, 0, 0, 0, 0));
         getStats().put(PositionCodes.AMF,
                 new PlayerStats(0, 0, 7, 0, 7, 0, -10, 0, 0, 7, 0, 7, -10, 0, 0, 0, 0));
         getStats().put(PositionCodes.SMF,
-                new PlayerStats(0, 0, 7, 0, 7, 0, -10, 0, 7, 7, 0, 7, -10, 0, 0, 0, 0));
+                new PlayerStats(0, 7, 0, 0, 7, 0, -10, -10, 0, 7, 7, 0, 0, 0, 0, 0, 0));
         getStats().put(PositionCodes.CMF,
-                new PlayerStats(0, 7, 0, 0, 7, 0, -10, -10, 7, 7, 7, 7, 0, 0, 0, 0, 0));
+                new PlayerStats(0, 0, 0, 7, 7, 0, -10, -10, 0, 7, 0, 7, 0, 0, 0, 0, 0));
         getStats().put(PositionCodes.DMF,
                 new PlayerStats(0, 7, 0, 0, 0, 7, 0, 7, -10, -10, 0, 0, 0, 7, 0, 0, 0));
         getStats().put(PositionCodes.SB,
