c2ddd6b5845cb5b287d89e1a0cde4ef70b8d36b6
neikeq <ignalfonsore@gmail.com>
2015-01-16 14:15:24 +0100

    Update run.sh and run.bat.

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@
 
 TITLE KicksEmulator
 
-java -classpath lib/commons-dbcp2-2.0.1.jar;lib/commons-pool2-2.2.jar;lib/commons-logging-1.1.3.jar;lib/mysql-connector-java-5.1.30-bin.jar;lib/netty-all-4.0.24.Final.jar;out/production/KicksEmu com/neikeq/kicksemu/KicksEmu %1
+java -classpath lib/commons-dbcp2-2.0.1.jar;lib/commons-pool2-2.2.jar;lib/commons-logging-1.1.3.jar;lib/mysql-connector-java-5.1.30-bin.jar;lib/netty-all-4.0.24.Final.jar;lib/opencsv-3.1.jar;out/production/KicksEmu com/neikeq/kicksemu/KicksEmu %1
 
 PAUSE
