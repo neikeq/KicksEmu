b8a563725566c2d8a92e677cc31d1d57debf8c59
neikeq <ignalfonsore@gmail.com>
2015-01-18 20:56:04 +0100

    Leave expiration timestamp as zero if it is permanent.

--- src/main/java/com/neikeq/kicksemu/game/inventory/InventoryUtils.java
+++ src/main/java/com/neikeq/kicksemu/game/inventory/InventoryUtils.java
@@ -9,6 +9,10 @@ import java.util.stream.Collectors;
 public class InventoryUtils {
 
     public static Timestamp expirationToTimestamp(Expiration expiration) {
+        if (expiration == Expiration.DAYS_PERM) {
+            return new Timestamp(0);
+        }
+
         int days = expiration.toInt() % 1000;
 
         return DateUtils.toTimestamp(DateUtils.addDays(DateUtils.getSqlDate(), days));
