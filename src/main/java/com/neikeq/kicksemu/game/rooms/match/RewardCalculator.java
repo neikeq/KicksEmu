b7738602769a95754e8e10458dbfb56a5c16aef7
neikeq <ignalfonsore@gmail.com>
2015-01-19 13:38:42 +0100

    Fix reward calculation bugs and exploits.

--- src/main/java/com/neikeq/kicksemu/game/rooms/match/RewardCalculator.java
+++ src/main/java/com/neikeq/kicksemu/game/rooms/match/RewardCalculator.java
@@ -7,17 +7,21 @@ import com.neikeq.kicksemu.game.rooms.Room;
 public class RewardCalculator {
 
     public static int calculateReward(PlayerResult result, Room room, short countdown) {
+        short votePoints = result.getVotePoints() > 100 ? 100 : result.getVotePoints();
+
         switch (room.getTrainingFactor()) {
             case 0:
                 if (Configuration.getBoolean("game.rewards.practice") && countdown <= 0) {
-                    return (int)(12 * (float)(result.getVotePoints() / 10));
+                    return (int)(12 * (float)(votePoints / 10));
                 }
+
+                return 0;
             case 6:
-                return (int)(12 * (float)(result.getVotePoints() / 10));
+                return (int)(12 * (float)(votePoints / 10));
             case 8:
-                return (int)(18 * (float)(result.getVotePoints() / 10));
+                return (int)(18 * (float)(votePoints / 10));
             case 10:
-                return (int)(24 * (float)(result.getVotePoints() / 10));
+                return (int)(24 * (float)(votePoints / 10));
             default:
                 return 0;
         }
