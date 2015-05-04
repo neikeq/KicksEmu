06309b2b5520dc0905f0101cfb0f52faf4ac2714
neikeq <ignalfonsore@gmail.com>
2015-05-04 22:48:46 +0200

    gradle!

--- run.bat
+++ run.bat
@@ -2,7 +2,6 @@
 
 TITLE KicksEmulator
 
-java -classpath lib/commons-dbcp2-2.0.1.jar;lib/commons-pool2-2.2.jar;lib/commons-logging-1.1.3.jar;lib/mysql-connector-java-5.1.30-bin.jar;lib/netty-all-4.0.24.Final.jar;lib/opencsv-3.1.jar;out/production/KicksEmu com/neikeq/kicksemu/KicksEmu %1
+gradlew.bat run -Pargs="%1"
 
 PAUSE
-
