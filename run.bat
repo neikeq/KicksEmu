da1cbe0aea108f1f860cd129c1a2c8bf81a77a11
neikeq <ignalfonsore@gmail.com>
2015-05-06 22:14:41 +0200

    Again... updated run.sh and run.bat to be more flexible.

--- run.bat
+++ run.bat
@@ -2,6 +2,8 @@
 
 TITLE KicksEmulator
 
-gradlew.bat run -Pargs="%1" $2
+for /f "tokens=1,* delims= " %%a in ("%*") do set ARGS=%%b
+
+gradlew.bat run -Pargs="%1" ARGS
 
 PAUSE
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./gradlew run -Pargs="$1" $2
+./gradlew run -Pargs="$1" ${@:2}
 
 read -n1 -r -p "Press any key to continue..." key
